95378bec3e77b77a1b84d8bac9cc5297020062be
Tasuku Suzuki <stasuku@gmail.com>
2015-05-27 13:04:56 +0900

    add a subclass of QObject

--- property/main.cpp
+++ property/main.cpp
@@ -1,8 +1,42 @@
 #include <QtCore/QCoreApplication>
+#include <QtCore/QDebug>
+
+class Object : public QObject
+{
+    Q_OBJECT
+public:
+    Object(QObject *parent = Q_NULLPTR)
+        : QObject(parent), m_value(0) {}
+
+    int value() const { return m_value; }
+
+public slots:
+    void setValue(int value) {
+        if (m_value == value) return;
+        m_value = value;
+        emit valueChanged(value);
+    }
+
+signals:
+    void valueChanged(int value);
+
+private:
+    int m_value;
+};
 
 int main(int argc, char *argv[])
 {
     QCoreApplication app(argc, argv);
 
-    return app.exec();
+    Object object;
+    QObject::connect(&object, &Object::valueChanged, [](int value) {
+        qDebug() << value;
+    });
+
+    qDebug() << object.value();
+    object.setValue(100);
+    qDebug() << object.value();
+    return 0;
 }
+
+#include "main.moc"
